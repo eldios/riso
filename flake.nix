6c974dc5d6d36e76414a9c13cb00e7a591452428
Emanuele Calo <emanuele.lele.calo@gmail.com>
2026-08-18 02:56:28 +0200

    chore(release): v0.2.1
    The Omarchy drop-in applies themes end to end.

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       risoPackage = pkgs:
         pkgs.rustPlatform.buildRustPackage {
           pname = "riso";
-          version = "0.2.0";
+          version = "0.2.1";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
 
